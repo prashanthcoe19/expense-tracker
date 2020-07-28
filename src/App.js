cda611d09472b1e859d38c7b05825b377d36d54d
Prashant <prashant.niroula67@gmail.com>
2020-07-29 00:09:27 +0545

    ui setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpenses from './components/IncomeExpenses';
+import TranscationList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
 import './App.css';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Header />
+      <div className='container'>
+        <Balance />
+        <IncomeExpenses />
+        <TranscationList />
+        <AddTransaction />
+      </div>
+    </div>
+  );
 }
 
 export default App;
